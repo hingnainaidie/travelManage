1d0dbc09908ab42fb8d7e047b21c2f2528df59c1
hzy0705 <675849402@qq.com>
2022-06-25 09:27:43 +0800

    my_case

--- src/views/visiter/my_msg/my_case.vue
+++ src/views/visiter/my_msg/my_case.vue
@@ -1,10 +1,64 @@
 <template>
 	<div>
-		我的报案
+		<el-row>
+		  <el-button type="primary" @click="newCase()">新建</el-button>
+		</el-row>
+		<el-table
+		      :data="tableData"
+		      style="width: 100%">
+		      <el-table-column
+		        prop="case_id"
+		        label="报案编号"
+		        width="180">
+		      </el-table-column>
+		      <el-table-column
+		        prop="case_status"
+		        label="报案处理状态"
+		        width="180">
+		      </el-table-column>
+		      <el-table-column
+		        prop="description"
+		        label="报案事件描述">
+		      </el-table-column>
+			  <el-table-column>
+			  					  <el-link :underline="false" @click="check_case()()">查看详情<i class="el-icon-view el-icon--right"></i> </el-link>
+			  </el-table-column>
+		</el-table>
 	</div>
 </template>
 
 <script>
+	export default {
+	  data() {
+	    return {
+	      tableData: [{
+	        case_id: 'ca000001',
+	        case_status: '已处理',
+	        description: 'xxx'
+	      }, {
+	        case_id: 'ca000002',
+	        case_status: '处理中',
+	        description: 'xxx'
+	      }, {
+	        case_id: 'ca000003',
+	        case_status: '未处理',
+	        description: 'xxx'
+	      }]
+	    }
+	  },
+	  methods: {
+	    newCase(){
+	  	  this.$router.push({
+	  	    path: "/visiter_mng/my_msg/new_case"
+	  	  })
+	    },
+		check_case(){
+		  this.$router.push({
+		    path: "/visiter_mng/my_msg/check_case"
+		  })
+		}
+	  }
+	}
 </script>
 
 <style>
