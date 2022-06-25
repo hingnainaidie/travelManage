4e5891304314ab3b1d98115474487f5db975a488
hzy0705 <675849402@qq.com>
2022-06-25 09:40:30 +0800

    staff_solve

--- src/views/staff/events_solve/complain_solve.vue
+++ src/views/staff/events_solve/complain_solve.vue
@@ -1,11 +1,55 @@
 <template>
 	<div>
-		投诉处理
+		<el-table
+		      :data="tableData"
+		      style="width: 100%">
+		      <el-table-column
+		        prop="complaint_id"
+		        label="投诉编号"
+		        width="180">
+		      </el-table-column>
+		      <el-table-column
+		        prop="complaint_status"
+		        label="投诉处理状态"
+		        width="180">
+		      </el-table-column>
+		      <el-table-column
+		        prop="description"
+		        label="投诉事件描述">
+		      </el-table-column>
+			 <el-table-column>
+			 	<el-link :underline="false" @click="check_complain()">查看详情<i class="el-icon-view el-icon--right"></i> </el-link>
+			 </el-table-column>
+		</el-table>
 	</div>
 </template>
-
 <script>
+		    export default {
+		      data() {
+		        return {
+		          tableData: [{
+		            complaint_id: 'co000001',
+		            complaint_status: '已处理',
+		            description: 'xxx'
+		          }, {
+		            complaint_id: 'co000002',
+		            complaint_status: '处理中',
+		            description: 'xxx'
+		          }, {
+		            complaint_id: 'co000003',
+		            complaint_status: '未处理',
+		            description: 'xxx'
+		          }]
+		        }
+		      },
+			  methods: {
+				check_complain(){
+				  this.$router.push({
+				    path: "/staff_mng/events_solve/check_complain"
+				  })
+				}
+			  }
+		    }
 </script>
-
 <style>
 </style>
